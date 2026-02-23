d61ad83ca1d0b4398d3ec806ade205bcaf72fd6e
DAVEfm12 <davemoras02@gmail.com>
2026-02-23 17:56:27 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function nextPage(){
+window.scrollTo({
+top: window.innerHeight,
+behavior: 'smooth'
+});
+}
+
+window.onscroll=function(){
+let winScroll=document.documentElement.scrollTop;
+let height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+let scrolled=(winScroll/height)*100;
+document.getElementById("progressBar").style.width=scrolled+"%";
+}
